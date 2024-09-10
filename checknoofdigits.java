b070f4fb96d138c1cf6c490bdfe4059e72db86a9
hasnain-69 <knishk421@gmail.com>
2024-09-10 15:55:59 +0530

    Add files via upload

---
+++ checknoofdigits.java
@@ -0,0 +1,19 @@
+public class checknoofdigit{
+ public static void main(String args{})
+ {
+    int x = 21;
+
+if(x>0 && x<10)
+{System.out.println("Single digit")}
+
+else if(x>=10 && x<100)
+{System.out.println("double digit")}
+
+else if(x>=100) 
+{System.out.println("Multiple Digits")}   
+
+else {System.out.println("Multiple Digits")}
+
+
+ }
+}
